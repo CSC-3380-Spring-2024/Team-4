07b685fdede57aef85c27ea09e178772531785a0
cdbelala <cdbelala@gmail.com>
2024-05-03 14:19:11 -0500

    Full re - push of my version of the repo with movment finished, will conitnue to work on it today

--- Cryptid_Royale copy/Cards/ExitButton.gd
+++
@@ -1,5 +0,0 @@
-extends TextureButton
-
-func _pressed():
-    var curr = get_parent().get_parent().get_parent()
-    curr.visible = !curr.visible
--- Cryptid_Royale copy/Cards/PlayerDeck.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-static var CardList = ["Barghest","Bigfoot","Megalodon","Mapinguari","Chupacabra"]
-
-func change(rep: String, with: String):
-    var index = CardList.search(rep)
-    CardList[index] = with
